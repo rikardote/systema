a3713248d9cdb0bb3bf735bfb51217491b51d1cd
rikardote <ricarmenta@gmail.com>
2015-10-07 00:03:45 -0700

    added db sql

--- sistema (2).sql
+++ sistema (2).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 04-10-2015 a las 12:28:35
+-- Tiempo de generación: 07-10-2015 a las 00:02:24
 -- Versión del servidor: 5.5.43-0ubuntu0.14.04.1
 -- Versión de PHP: 5.6.10-1+deb.sury.org~trusty+1
 
@@ -62,30 +62,19 @@ CREATE TABLE IF NOT EXISTS `captura_incidencias` (
   `incidencia_id` int(3) NOT NULL,
   `fecha_inicial` date NOT NULL,
   `fecha_final` date NOT NULL,
+  `periodo_id` int(11) DEFAULT NULL,
   `token` varchar(64) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `capturada` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `num_empleado` (`empleado_id`,`incidencia_id`),
   KEY `incidencia_cod` (`incidencia_id`),
   KEY `qna` (`qna_id`),
   KEY `qna_id` (`qna_id`),
-  KEY `qna_id_2` (`qna_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
-
---
--- Volcado de datos para la tabla `captura_incidencias`
---
-
-INSERT INTO `captura_incidencias` (`id`, `qna_id`, `empleado_id`, `incidencia_id`, `fecha_inicial`, `fecha_final`, `token`, `created_at`, `updated_at`) VALUES
-(19, 5, 2, 3, '2015-09-12', '2015-09-12', '', '2015-09-29 16:30:18', '2015-09-29 23:30:18'),
-(20, 10, 3, 4, '2015-09-12', '2015-09-12', '39eeb9460e20c857ad7d9da92d1df98f9000b917', '2015-09-29 17:15:55', '2015-09-30 00:15:55'),
-(21, 5, 2, 1, '2015-09-12', '2015-09-12', 'bb1b4e424dc7cf89436b73927a5f7fb4ed69842e', '2015-09-29 17:18:53', '2015-09-30 00:18:53'),
-(22, 9, 3, 3, '2015-09-12', '2015-09-12', '0a190fe0c474b17e298da11b30a71eab000cb200', '2015-09-30 16:12:53', '2015-09-30 16:12:53'),
-(23, 6, 3, 2, '2015-09-12', '2015-09-12', '7e33133aeb343b61c307486d9df2ef1a0bf224d3', '2015-09-30 17:10:33', '2015-09-30 17:10:33'),
-(28, 5, 10, 7, '2015-09-12', '2015-09-12', 'cbc98589531521389234b581637f1fe36394dd44', '2015-09-30 16:31:08', '2015-09-30 23:31:08'),
-(29, 10, 10, 8, '2015-09-12', '2015-09-12', '2ceac954470f6d732de37018b671998bc0172d47', '2015-09-30 16:37:47', '2015-09-30 23:37:47'),
-(30, 5, 3, 5, '2015-09-15', '2015-09-16', '0ec42d25deaa5f55c6c5cb192173b87743ef1a2d', '2015-09-30 22:10:42', '2015-10-01 05:10:42');
+  KEY `qna_id_2` (`qna_id`),
+  KEY `periodo_id` (`periodo_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=71 ;
 
 -- --------------------------------------------------------
 
@@ -109,16 +98,16 @@ CREATE TABLE IF NOT EXISTS `empleados` (
   KEY `adscripcion` (`adscripcion_id`),
   KEY `id` (`id`),
   KEY `id_2` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=32 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
 
 --
 -- Volcado de datos para la tabla `empleados`
 --
 
 INSERT INTO `empleados` (`id`, `num_empleado`, `nombres`, `apellido_pat`, `apellido_mat`, `adscripcion_id`, `activo`, `created_at`, `updated_at`) VALUES
-(2, 332618, 'HECTOR RICARDO', 'FUENTES', 'ARMENTA', 1, 1, '0000-00-00 00:00:00', '2015-10-04 08:47:42'),
+(2, 332618, 'HECTOR RICARDO', 'FUENTES', 'ARMENTA', 1, 1, '0000-00-00 00:00:00', '2015-10-06 21:53:30'),
 (3, 125259, 'MONICA', 'GUTIERREZ', 'SAMANIEGO', 7, 1, '0000-00-00 00:00:00', '2015-10-04 07:15:08'),
-(10, 225225, 'JOSE MARIA', 'FUENTES', 'ARMENTA', 3, 1, '2015-09-30 16:12:07', '2015-10-04 08:49:14'),
+(10, 225225, 'JOSE MARIA', 'FUENTES', 'ARMENTA', 3, 1, '2015-09-30 16:12:07', '2015-10-05 22:31:06'),
 (11, 147258, 'NANCY DANIXA', 'FUENTES', 'ARMENTA', 6, 1, '2015-09-30 16:58:34', '2015-10-04 05:44:35'),
 (12, 258369, 'ALEJANDRO', 'PRADO', 'SALCIDO', 6, 1, '2015-09-30 16:59:56', '2015-10-04 05:44:35'),
 (13, 159951, 'ALBERTO', 'AGUILAR', 'NEGRETE', 5, 1, '2015-10-01 17:08:53', '2015-10-04 05:44:35'),
@@ -131,8 +120,8 @@ INSERT INTO `empleados` (`id`, `num_empleado`, `nombres`, `apellido_pat`, `apell
 (21, 045875, 'RUBEN', 'GARCIA', 'ROSALES', 5, 1, '2015-10-03 15:39:54', '2015-10-04 05:44:35'),
 (22, 025478, 'MANUEL', 'SERRANO', 'ARIAS', 1, 1, '2015-10-03 17:38:50', '2015-10-04 07:14:54'),
 (23, 336699, 'GILBERTO', 'GLESS', 'GUZMAN', 7, 1, '2015-10-04 00:21:03', '2015-10-04 07:21:03'),
-(30, 456456, 'DFSGSDFGSD', 'FGDSFG', 'SDFGDSFG', 3, 1, '2015-10-04 00:36:12', '2015-10-04 07:36:12'),
-(31, 067678, 'DFASDFSDF', 'ASDFASDFASDF', 'ASDFSADF', 6, 1, '2015-10-04 00:37:47', '2015-10-04 07:38:49');
+(24, 033654, 'NANCY DANIXA', 'ASDFASDFASDF', 'ARMENTA', 11, 1, '2015-10-05 15:17:19', '2015-10-05 22:17:19'),
+(25, 245890, 'HOLA', 'ASDFASDFASDF', 'ARMENTA', 2, 1, '2015-10-05 15:19:37', '2015-10-05 22:19:37');
 
 -- --------------------------------------------------------
 
@@ -150,7 +139,7 @@ CREATE TABLE IF NOT EXISTS `incidencias` (
   KEY `incidencia_cod` (`incidencia_cod`),
   KEY `incidencia_cod_2` (`incidencia_cod`),
   KEY `incidencia_cod_3` (`incidencia_cod`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Volcado de datos para la tabla `incidencias`
@@ -166,7 +155,38 @@ INSERT INTO `incidencias` (`id`, `incidencia_cod`, `inc_descripcion`, `created_a
 (8, 10, 'FALTA', '2015-09-30 16:35:27', '2015-10-03 07:17:52'),
 (9, 07, 'RETARDO POR CONSULTA MEDICA', '2015-10-01 19:43:03', '2015-10-03 07:17:33'),
 (10, 02, 'RETARDO DE 30 A 59 MINUTOS', '2015-10-01 21:39:21', '2015-10-03 07:16:16'),
-(15, 19, 'OMISION DE SALIDA', '2015-10-03 00:04:17', '2015-10-03 07:18:21');
+(15, 19, 'OMISION DE SALIDA', '2015-10-03 00:04:17', '2015-10-03 07:18:21'),
+(16, 60, 'VACACIONES ORDINARIAS', '2015-10-06 17:53:28', '2015-10-07 00:53:28');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `periodos`
+--
+
+CREATE TABLE IF NOT EXISTS `periodos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `period` int(2) unsigned zerofill NOT NULL,
+  `year` int(4) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `activo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`),
+  KEY `id_2` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+-- Volcado de datos para la tabla `periodos`
+--
+
+INSERT INTO `periodos` (`id`, `period`, `year`, `created_at`, `updated_at`, `activo`) VALUES
+(4, 01, 2014, '0000-00-00 00:00:00', '2015-10-07 00:36:57', 0),
+(5, 02, 2014, '0000-00-00 00:00:00', '2015-10-07 00:36:54', 0),
+(7, 01, 2015, '2015-10-06 17:26:10', '2015-10-07 00:37:02', 1),
+(8, 02, 2015, '2015-10-06 17:26:46', '2015-10-07 00:37:02', 1),
+(9, 05, 2014, '2015-10-06 17:28:19', '2015-10-07 00:36:54', 0),
+(10, 00, 0, '0000-00-00 00:00:00', '2015-10-07 03:24:33', 0);
 
 -- --------------------------------------------------------
 
@@ -204,9 +224,9 @@ INSERT INTO `qnas` (`id`, `qna_mes`, `qna_year`, `qna_descripcion`, `activa`, `c
 (13, 07, 2015, '1ERA DE ABRIL', 0, '2015-10-01 21:33:56', '2015-10-04 06:55:03'),
 (14, 08, 2015, '2DA DE ABRIL', 0, '2015-10-02 15:58:40', '2015-10-03 21:38:24'),
 (36, 01, 2015, '1ERA DE ENERO', 0, '2015-10-03 00:39:58', '2015-10-03 21:28:17'),
-(38, 10, 2015, '2DA DE MAYO', 0, '2015-10-03 14:20:55', '2015-10-03 23:50:10'),
-(39, 11, 2015, '1ERA DE JUNIO', 1, '2015-10-03 16:42:09', '2015-10-04 08:04:02'),
-(40, 12, 2015, '2DA DE JUNIO', 1, '2015-10-03 16:52:17', '2015-10-04 00:12:08');
+(38, 10, 2015, '2DA DE MAYO', 0, '2015-10-03 14:20:55', '2015-10-06 07:03:49'),
+(39, 11, 2015, '1ERA DE JUNIO', 1, '2015-10-03 16:42:09', '2015-10-07 00:36:39'),
+(40, 12, 2015, '2DA DE JUNIO', 1, '2015-10-03 16:52:17', '2015-10-06 06:28:32');
 
 --
 -- Restricciones para tablas volcadas
@@ -218,7 +238,8 @@ INSERT INTO `qnas` (`id`, `qna_mes`, `qna_year`, `qna_descripcion`, `activa`, `c
 ALTER TABLE `captura_incidencias`
   ADD CONSTRAINT `captura_incidencias_ibfk_3` FOREIGN KEY (`qna_id`) REFERENCES `qnas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `captura_incidencias_ibfk_4` FOREIGN KEY (`empleado_id`) REFERENCES `empleados` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `captura_incidencias_ibfk_5` FOREIGN KEY (`incidencia_id`) REFERENCES `incidencias` (`id`);
+  ADD CONSTRAINT `captura_incidencias_ibfk_5` FOREIGN KEY (`incidencia_id`) REFERENCES `incidencias` (`id`),
+  ADD CONSTRAINT `captura_incidencias_ibfk_6` FOREIGN KEY (`periodo_id`) REFERENCES `periodos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `empleados`
